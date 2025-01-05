673dcfc345986fbde73e2f9c84ccc61ef1210758
Ayush Sharma <72184607+12521198-Ayush@users.noreply.github.com>
2025-01-05 17:09:08 +0530

    Ayush

--- src/app/otp/OtpInput.jsx
+++ src/app/otp/OtpInput.jsx
@@ -28,28 +28,29 @@ const OtpInput = ({ maxLength = 6, handleOtpChange }) => {
 
   return (
     <div className="flex flex-col items-center space-y-4">
-      {/* Enter OTP Label */}
-      <label className="text-gray-700 dark:text-gray-300 text-lg font-medium sm:text-xl">
-        Enter OTP
-      </label>
-
-      {/* OTP Input Fields */}
-      <div className="flex justify-center space-x-2 sm:space-x-4">
-        {otp.map((digit, index) => (
-          <input
-            key={index}
-            id={`otp-${index}`}
-            type="text"
-            maxLength={1}
-            value={digit}
-            onChange={(e) => handleInputChange(e.target.value, index)}
-            onKeyDown={(e) => handleKeyDown(e, index)}
-            className="border-gray-300 bg-gray-100 dark:border-gray-500 dark:bg-gray-800 h-12 w-12 rounded-md border-2 text-center text-xl text-black outline-none transition focus:border-primary focus:ring-2 focus:ring-primary/50 dark:text-white sm:h-14 sm:w-14"
-            placeholder="•"
-          />
-        ))}
-      </div>
+    {/* Enter OTP Label */}
+    <label className="text-gray-700 dark:text-gray-300 text-lg font-medium sm:text-xl">
+      Enter OTP
+    </label>
+  
+    {/* OTP Input Fields */}
+    <div className="flex justify-center space-x-2 sm:space-x-4">
+      {otp.map((digit, index) => (
+        <input
+          key={index}
+          id={`otp-${index}`}
+          type="text"
+          maxLength={1}
+          value={digit}
+          onChange={(e) => handleInputChange(e.target.value, index)}
+          onKeyDown={(e) => handleKeyDown(e, index)}
+          className="border-gray-300 bg-gray-100 dark:border-gray-400 dark:bg-gray-500 h-6 w-6 min-w-[18px] rounded-md border-2 text-center text-xl text-black outline-none transition focus:border-primary focus:ring-2 focus:ring-primary/50 dark:text-white sm:h-14 sm:w-10"
+          placeholder="•"
+        />
+      ))}
     </div>
+  </div>
+  
   );
 };
 
