c1961fdc2f89586ca1f86a7d0231d386bd75c529
Ayush Sharma <72184607+12521198-Ayush@users.noreply.github.com>
2025-01-23 23:29:56 +0530

    Ayush

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -431,7 +431,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                   return (
                     <React.Fragment>
                       <Link
-                        href="/emailS"
+                        href="/emails"
                         className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${(pathname === "/flats-residents" ||
                           pathname.includes("manage-helpers")) &&
                           "bg-graydark dark:bg-meta-4"
@@ -470,7 +470,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                         <ul className="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
                           <li>
                             <Link
-                              href="/emailS/inbox"
+                              href="/emails/inbox"
                               className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${pathname === "/manage-resident-owner" && "text-white"
                                 }`}
                             >
